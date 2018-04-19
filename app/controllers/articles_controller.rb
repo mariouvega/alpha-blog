9ea8e5275e6e0083469afaf6ba0b92cfd57d7a5d
mariouvega <mario.vega014@gmail.com>
2018-04-19 17:48:26 -0400

    fix create, add flash, fixed error msgs and created show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,27 @@ class ArticlesController < ApplicationController
 	def create
 		# render plain: params[:article].inspect # shows it like a JSON object
 		@article = Article.new(article_params) # Remember we have to whitelist our params, that can be done and re-used in the private section
-		@article.save
-		redirect_to articles_show(@article)
+		# @article.save # DONT NEED THIS ANYMORE, WEIRD THO!!!
+
+		# redirect_to article_path(@article) # This redirects the user to the show action of the article after creating a new article
+
+		if @article.save
+			flash[:notice] = "Article was successfully created!"
+			redirect_to article_path(@article)
+		else
+			render 'new' # or it can be --> :new
+		end
+
+	end
+
+	def show
+		@article = Article.find(params[:id])
 	end
 
 	private
 
 	def article_params
-		params.require(:article).permit(:title, :description)
+		params.require(:article).permit(:title, :description) # THESE ARE STRONG PARAMS AND IS WHAT ALLOWS RAILS TO CREATE A NEW ARTICLE BECAUSE WE WHITE LIST THE PARAMS
 	end
 
 end
