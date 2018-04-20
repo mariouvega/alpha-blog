283a4ce4c4f14a92edb36baf30011d14e4b8e88d
mariouvega <mario.vega014@gmail.com>
2018-04-20 10:57:27 -0400

    Add edit update show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,23 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = Article.all
+	end
+
 	def new
-		@article = Article.new
+		@article = Article.new # THIS IS HANDLED BY THE CREATE ACTION
+	end
+
+	def edit
+		@article = Article.find(params[:id]) # THIS HANDLED BY THE UPDATE ACTION
 	end
 
 	def create
 		# render plain: params[:article].inspect # shows it like a JSON object
-		@article = Article.new(article_params) # Remember we have to whitelist our params, that can be done and re-used in the private section
 		# @article.save # DONT NEED THIS ANYMORE, WEIRD THO!!!
-
 		# redirect_to article_path(@article) # This redirects the user to the show action of the article after creating a new article
+
+		@article = Article.new(article_params) # Remember we have to whitelist our params, that can be done and re-used in the private section
 
 		if @article.save
 			flash[:notice] = "Article was successfully created!"
@@ -22,6 +30,16 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
 	end
 
 	private
